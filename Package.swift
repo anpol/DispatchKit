93c32eb7e72979dcc396050525cffa40aa7476a2
ElvishJerricco <elvishjerricco@gmail.com>
2015-12-07 15:21:37 -0500

    Include Package.swift
    This is the file that the Swift package manager uses to know
    information about the package.

---
+++ Package.swift
@@ -0,0 +1,10 @@
+//
+//  Package.swift
+//  DispatchKit
+//
+//  Copyright © 2015 Andrei Polushin. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(name: "DispatchKit")
