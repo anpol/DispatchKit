32c895f152536d8c66fe57b4b353eef44fac1521
ElvishJerricco <elvishjerricco@gmail.com>
2015-12-07 14:39:17 -0500

    Pure swift ARC bridging convenience functions

---
+++ DispatchKit/Bridge.swift
@@ -0,0 +1,26 @@
+//
+//  Bridge.swift
+//  DispatchKit
+//
+//  Copyright (c) 2015 Andrei Polushin. All rights reserved.
+//
+
+func bridge<T: AnyObject>(obj: T) -> UnsafePointer<Void> {
+    return UnsafePointer(Unmanaged.passUnretained(obj).toOpaque())
+}
+
+func bridge<T: AnyObject>(ptr: UnsafePointer<Void>) -> T {
+    return Unmanaged<T>.fromOpaque(COpaquePointer(ptr)).takeUnretainedValue()
+}
+
+func bridgeRetained<T: AnyObject>(obj: T) -> UnsafePointer<Void> {
+    return UnsafePointer(Unmanaged.passRetained(obj).toOpaque())
+}
+
+func bridgeTransfer<T: AnyObject>(ptr: UnsafePointer<Void>) -> T {
+    return Unmanaged<T>.fromOpaque(COpaquePointer(ptr)).takeRetainedValue()
+}
+
+func release(ptr: UnsafePointer<Void>) {
+    Unmanaged<AnyObject>.fromOpaque(COpaquePointer(ptr)).release()
+}
